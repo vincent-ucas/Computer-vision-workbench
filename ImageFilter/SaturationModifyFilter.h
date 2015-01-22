cebd4745ab75e84a0787bd79bd73ffdb0d79b365
vincent he <vincent.ucas@gmail.com>
2015-01-22 21:45:21 +0800

    add saturation modify

---
+++ ImageFilter/SaturationModifyFilter.h
@@ -0,0 +1,53 @@
+
+
+#if !defined(SaturationModifyFilter_H)
+#define SaturationModifyFilter_H
+
+#include "IImageFilter.h"
+
+
+namespace HM_ImageFilter{
+
+class SaturationModifyFilter : public IImageFilter{
+
+public:
+	float SaturationFactor;
+	SaturationModifyFilter(float in_satFact = 0.5f){
+		SaturationFactor = in_satFact;
+	};
+
+	virtual Image process(Image imageIn)
+	{
+		float saturation = SaturationFactor + 1;
+    	float negosaturation = 1 - saturation;
+        int r, g, b;
+        for (int x = 0; x < imageIn.getWidth(); x++) {
+            for (int y = 0; y < imageIn.getHeight(); y++) {
+            	 r = imageIn.getRComponent(x, y);
+                 g = imageIn.getGComponent(x, y);
+                 b = imageIn.getBComponent(x, y);  
+                 
+                 float nego1 = negosaturation * 0.2126;
+                 float ngeo2 = nego1 + saturation;
+                 float ngeo3 = negosaturation * 0.7152;
+                 float nego4 = ngeo3 + saturation;
+                 float nego5 = negosaturation * 0.0722;
+                 float nego6 = nego5 + saturation;
+                 float rr = ((r * ngeo2) + (g * ngeo3)) + (b * nego5);
+                 float gg = ((r * nego1) + (g * nego4)) + (b * nego5);
+                 float bb = ((r * nego1) + (g * ngeo3)) + (b * nego6);
+                 r = SAFECOLOR(rr);
+                 g = SAFECOLOR(gg);
+                 b = SAFECOLOR(bb);
+                 imageIn.setPixelColor(x,y,r,g,b);
+             }
+        } 
+#ifndef WIN32 //only for apple ios
+		imageIn.copyPixelsFromBuffer();
+#endif
+        return imageIn;
+	}
+};
+
+}// namespace 
+#endif
